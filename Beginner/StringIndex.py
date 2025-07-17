87cd567e45d8a2b0532100c6c706df933204bed1
John Seidl <jseidl2@wgu.edu>
2025-07-17 16:19:51 -0500

    Updated

--- Beginner/StringIndex.py
+++ Beginner/StringIndex.py
@@ -3,7 +3,6 @@ in_string = str(input())
 print(f'Original string is {in_string}')
 print(f'Print only even characters below:')
 
-length = len(in_string)
 
 for char in range(len(in_string)):
     if char % 2 == 0:
