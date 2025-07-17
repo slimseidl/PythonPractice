c8d639f57a3fb631d7b6d7a16d5e5286a6953a34
John Seidl <jseidl2@wgu.edu>
2025-07-17 15:31:31 -0500

    Exercise 1 file

---
+++ Beginner/Ex1.py
@@ -0,0 +1,8 @@
+num1 = int(input())
+num2 = int(input())
+product = num1 * num2
+
+if product < 1000:
+    print(f'The product of number 1 and number2 is {product}')
+else:
+    print(f'The product for the two values given is too high')
